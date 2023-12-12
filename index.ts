4a375107d5fcb6d7975f7c948c5531cfc0b2e769
thislife_won <sw8744@naver.com>
2023-12-12 17:06:03 +0900

    Edit README

--- index.ts
+++ index.ts
@@ -7,14 +7,24 @@ const connection = mysql.createConnection(process.env.DATABASE_URL);
 const app = express();
 
 app.get('/', (req: any, res: any) => {
-    res.send('OSU! API V1.0');
+    res.status(200).send('OSU! API V1.0');
+});
+
+app.get('/getall', (req: any, res: any) => {
+    connection.query(
+        'SELECT * FROM csat',
+        (err: any, results: any, fields: any) => {
+            res.status(200).send(results);
+        }
+    );
 });
 
 app.get('/get', (req: any, res: any) => {
     connection.query(
         'SELECT * FROM csat',
         (err: any, results: any, fields: any) => {
-            res.send(results);
+            let n = Math.random() * results.length;
+            res.status(200).send(results[Math.floor(n)]);
         }
     );
 });
