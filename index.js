b22b6d91d61fa4140f6f798bf306f997b04ad8cc
thislife_won <sw8744@naver.com>
2023-12-12 17:10:50 +0900

    Build

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 "use strict";
 const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).send('OSU! API V1.0');
+});
+app.get('/getall', (req, res) => {
+    connection.query('SELECT * FROM csat', (err, results, fields) => {
+        res.status(200).send(results);
+    });
+});
+app.get('/get', (req, res) => {
+    connection.query('SELECT * FROM csat', (err, results, fields) => {
+        let n = Math.random() * results.length;
+        res.status(200).send(results[Math.floor(n)]);
+    });
+});
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found');
 });
 app.listen(5500);
